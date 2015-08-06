e82462fd94705dc6390b7958ac0ecc16a8815dae
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-08-06 23:29:15 +0300

    Adds basic solution to the reverse problem using the while loop

--- challenge-1/00-reverse.rb
+++ challenge-1/00-reverse.rb
@@ -18,6 +18,26 @@ end
 # These are tests to check that your code is working. After writing
 # your solution, they should all print true.
 
+puts('reverse("abc") == "cba": ' + (reverse("abc") == "cba").to_s)
+puts('reverse("a") == "a": ' + (reverse("a") == "a").to_s)
+puts('reverse("") == "": ' + (reverse("") == "").to_s)
+
+def reverse(string)
+  new_string = ''
+
+  i = string.length - 1
+
+  while i >= 0
+    new_string += string[i]
+    i -= 1
+  end
+
+  new_string
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
 puts(
   'reverse("abc") == "cba": ' + (reverse("abc") == "cba").to_s
 )
