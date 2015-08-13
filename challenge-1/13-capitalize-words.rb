43ff1505b6623c3f293f52b067c16e83a37a0334
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-08-13 05:32:27 +0300

    Adds solution for capitalize words

--- challenge-1/13-capitalize-words.rb
+++ challenge-1/13-capitalize-words.rb
@@ -9,6 +9,26 @@
 # Difficulty: medium.
 
 def capitalize_words(string)
+
+  # Split the string to get an array
+  words = string.split(" ")
+
+  # Create a final array to store the capitalized words
+  final_array = []
+
+  # Now loop over each word in the array
+  i = 0
+
+  until i == words.length
+  # Get the first character of each word inside the array
+  # store it back inside the array
+
+    final_array.push(words[i][0].upcase!)
+     i = i + 1
+  end
+
+  final_array.join(" ")
+
 end
 
 # These are tests to check that your code is working. After writing
@@ -21,4 +41,8 @@ puts(
 puts(
   'capitalize_words("mike bloomfield") == "Mike Bloomfield": ' +
   (capitalize_words("mike bloomfield") == "Mike Bloomfield").to_s
+)
+puts(
+  'capitalize_words("i am layla") == "I Am Layla": ' +
+  (capitalize_words("i am layla") == "I Am Layla").to_s
 )
