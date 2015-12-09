8dc4c6616214824d9bd95334009f9d6f75f98203
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-09 23:28:15 +0300

    Adds disemvowel problem

---
+++ challenge-1/24-disemvowel.rb
@@ -0,0 +1,6 @@
+# Write a function disemvowel(string), which takes in a string,
+# and returns that string will all the vowels removed. Treat "y" as a consonant.
+
+disemvowel("foobar") == "fbr"
+disemvowel("ruby") == "rby"
+disemvowel("aeiou") == ""
