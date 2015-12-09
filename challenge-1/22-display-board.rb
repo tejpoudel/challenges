11af18e6a1f15fc998a00ee2dfcc9da780a2cd61
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-09 19:14:42 +0300

    Adds display_board tictactoe challenge

---
+++ challenge-1/22-display-board.rb
@@ -0,0 +1,11 @@
+# Objectives
+
+# Define a method that accepts an argument.
+# Use the argument within the method.
+# Read data from an array.
+# Print out a multi-line dynamic string using Interpolation
+
+# Question
+# Our goal is to build a method display_board that accepts a board array as an argument
+# and uses the data in that array to correctly print out an accurate representation of the
+# Tic Tac Toe board from the player's perspective based on the data in the array
