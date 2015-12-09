dbdba34c6d3ec92706270b853c9477b104d0e6b6
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-09 23:30:05 +0300

    Adds oddball problem

---
+++ challenge-1/25-oddball.rb
@@ -0,0 +1,6 @@
+# Write a function oddball_sum(numbers), which takes in an array of integers
+# and returns the sum of all the odd elements.
+
+oddball_sum([1,2,3,4,5]) == 9 # => 1 + 3 + 5 == 9
+oddball_sum([0,6,4,4]) == 0
+oddball_sum([1,2,1]) == 2
