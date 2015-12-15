d5a04d358ef53364f94c7bf9b22229bbd221bc54
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-15 12:59:41 +0300

    Removes overkill solution for lukcy_sevens challenge

--- challenge-1/23-lucky-sevens.rb
+++ challenge-1/23-lucky-sevens.rb
@@ -3,45 +3,9 @@
 #if any three consecutive elements sum to 7.
 
 def lucky_sevens?(numbers)
-
-# Find out which 3 consecutive numbers add up to 7 in a given random array
-
-# # Steps
-# 1. Go through each item in the array, find the next 2 after it i.e in groups of 3
-# 2. Sum the current item and its next 2, i.e the group
-# 3. If they add up to 7 store them in a new array and then finally return true
-# 4. If they don't add up to 7, return false
-# 5. Repeat the process for each item in the array and its next 2
-
-  # First failing iteration
-  # numbers.each do |number|
-
-  #   sum_of_three = []
-  #   sum_of_three << numbers.values_at(number, number+1, number+2).reduce(:+)
-  #   sum_of_three.include(7) ? true : false
-  # end
-
-  # Second successful iteration
-
-  # Iterate through the given numbers array
-  # Get the first 3 items using values_at and the range of the first 3 numbers
-  # Sum them up and store in an array
-  # Remove the current item from the original numbers array
-  # repeats the process only if the number of items in the array is greater than or equal to 3
-  # This deals with the nil error.
-  # Return true if the sum of 7 exists in the computed array from summing 3 numbers
-
-
-  final_array = []
-
-  numbers.each do |number|
-    if numbers.length >= 3
-      sum_of_three = numbers.values_at(0..2).inject { |x, y| x + y }
-      final_array << sum_of_three
-      numbers.shift(number)
-    end
-  end
-
-  final_array.include?(7) ? true : false
-
 end
+
+print lucky_sevens?([2,1,5,1,0])
+print lucky_sevens?([0,-2,1,8])
+print lucky_sevens?([7,7,7,7])
+print lucky_sevens?([3,4,3,4])
