01626cdbcdee249e68f4ef430834de628d0d1355
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-16 05:18:33 +0300

    Finishes draft one of lucky_sevens challenge

--- challenge-1/23-lucky-sevens.rb
+++ challenge-1/23-lucky-sevens.rb
@@ -3,9 +3,26 @@
 #if any three consecutive elements sum to 7.
 
 def lucky_sevens?(numbers)
+
+  final_array = []
+
+  index = 0
+
+  while index < numbers.length
+    ## Need to catch an out of range exception here
+    sum = numbers[index] + numbers[index + 1] + numbers[index + 2]
+    final_array << sum
+    index += 1
+  end
+
+  puts final_array
+
+  final_array.include?(7)
+
 end
 
-print lucky_sevens?([2,1,5,1,0])
-print lucky_sevens?([0,-2,1,8])
-print lucky_sevens?([7,7,7,7])
-print lucky_sevens?([3,4,3,4])
+puts lucky_sevens?([2,1,5])
+puts lucky_sevens?([2,1,5,1,0])
+puts lucky_sevens?([0,-2,1,8])
+puts lucky_sevens?([7,7,7,7])
+puts lucky_sevens?([3,4,3,4])
