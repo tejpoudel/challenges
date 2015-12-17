ae4fa937f1c32832e20e640af0ac1e736aad3fd9
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-17 03:19:20 +0300

    Adds one-off-words problem

---
+++ challenge-1/27-one-off-words.rb
@@ -0,0 +1,10 @@
+# Create a method one_off_words that will take as arguments a string and an
+# array of valid words (word_list) and return an array of words from the
+# word list which are the same except for a single letter at a single position
+
+
+def one_off_words(str, word_list)
+end
+
+puts one_off_words('moor',['door', 'moot', 'boot', 'boots'])
+puts one_off_words('bart', ['fcrt', 'ockd', 'dart', 'fort'])
