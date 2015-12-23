5b67fd9d881b9c23e6498fcfaf2799adb4c16f9e
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-23 23:06:11 +0300

    Adds consecutive string question

---
+++ challenge-1/28-consecutive-string-count.rb
@@ -0,0 +1,3 @@
+# Create a method consecutive_string_count that will take a string as an arguments
+# and return a multi-dimensional array of consecutive letters with their count.
+# "aaabbcaabc" --> [[a, 3], [b, 2], [c,1],[a,2],[b,1],[c,1]
