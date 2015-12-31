b78d32996b156dc149a84c392a118f2fdc507a8b
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-31 12:40:26 +0300

    Adds question for ordered vowels challenge

--- challenge-2/03_ordered_vowels.rb
+++ challenge-2/03_ordered_vowels.rb
@@ -1,2 +1,12 @@
+# Write a method, `ordered_vowel_words(str)` that takes a string of
+# lowercase words and returns a string with just the words containing
+# all their vowels (excluding "y") in alphabetical order. Vowels may
+# be repeated (`"afoot"` is an ordered vowel word).
+#
+# You will probably want a helper method, `ordered_vowel_word?(word)`
+# which returns true/false if a word's vowels are ordered.
+#
+# Difficulty: 2/5
+
 def ordered_vowel_words(str)
 end
