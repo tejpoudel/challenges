1439ee936ddd9e7fc26d29da7e89a965b840deb6
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-31 12:41:45 +0300

    Adds question for nearest largest integer challenge

--- challenge-2/00_nearest_larger.rb
+++ challenge-2/00_nearest_larger.rb
@@ -1,2 +1,15 @@
+# Write a function, `nearest_larger(arr, i)` which takes an array and an
+# index.  The function should return another index, `j`: this should
+# satisfy:
+#
+# (a) `arr[i] < arr[j]`, AND
+# (b) there is no `j2` closer to `i` than `j` where `arr[i] < arr[j2]`.
+#
+# In case of ties (see example below), choose the earliest (left-most)
+# of the two indices. If no number in `arr` is larger than `arr[i]`,
+# return `nil`.
+#
+# Difficulty: 2/5
+
 def nearest_larger(arr, idx)
 end
