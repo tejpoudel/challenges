b4e9ae05b01e0cd79bb2529cb97b71682c3965eb
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-31 12:40:50 +0300

    Adds question for letter count challenge

--- challenge-2/02_letter_count.rb
+++ challenge-2/02_letter_count.rb
@@ -1,2 +1,8 @@
+# Write a function, `letter_count(str)` that takes a string and
+# returns a hash mapping each letter to its frequency. Do not include
+# spaces.
+#
+# Difficulty: 1/5
+
 def letter_count(str)
 end
