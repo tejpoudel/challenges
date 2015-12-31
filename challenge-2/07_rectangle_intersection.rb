272a2f3ba43516c6316c8597ca525d8655ea2a4f
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-31 12:39:38 +0300

    Adds question for rectangular intersection challenge

--- challenge-2/07_rectangle_intersection.rb
+++ challenge-2/07_rectangle_intersection.rb
@@ -1,2 +1,16 @@
+# Write a function, `rec_intersection(rect1, rect2)` and returns the
+# intersection of the two.
+#
+# Rectangles are represented as a pair of coordinate-pairs: the
+# bottom-left and top-right coordinates (given in `[x, y]` notation).
+#
+# Hint: You can calculate the left-most x coordinate of the
+# intersection by taking the maximum of the left-most x coordinate of
+# each rectangle. Likewise, you can calculate the top-most y
+# coordinate of the intersection by taking the minimum of the top most
+# y coordinate of each rectangle.
+#
+# Difficulty: 4/5
+
 def rec_intersection(rect1, rect2)
 end
