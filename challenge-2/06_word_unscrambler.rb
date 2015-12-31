d5218ea1e96442de0f3e37d89d8c04e9fa905c6f
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-31 12:46:48 +0300

    Adds question for word scrambler challenge

--- challenge-2/06_word_unscrambler.rb
+++ challenge-2/06_word_unscrambler.rb
@@ -1,2 +1,12 @@
+# Write a function word_unscrambler that takes two inputs: a scrambled
+# word and a dictionary of real words.  Your program must then output
+# all words that our scrambled word can unscramble to.
+#
+# Hint: To see if a string `s1` is an anagram of `s2`, split both
+# strings into arrays of letters. Sort the two arrays. If they are
+# equal, then they are anagrams.
+#
+# Difficulty: 3/5
+
 def word_unscrambler(str, words)
 end
