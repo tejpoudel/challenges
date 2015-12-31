825ca29c75f730660416cad03e604b43234986d2
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-31 12:39:07 +0300

    Adds question for bubble sort challenge

--- challenge-2/08_bubble_sort.rb
+++ challenge-2/08_bubble_sort.rb
@@ -1,2 +1,8 @@
+# Write a function `bubble_sort(arr)` which will sort an array of
+# integers using the "bubble sort"
+# methodology. (http://en.wikipedia.org/wiki/Bubble_sort)
+#
+# Difficulty: 3/5
+
 def bubble_sort(arr)
 end
