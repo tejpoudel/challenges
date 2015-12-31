7ca1d3a8b9ca0353d2240cfe5e717cfedf698077
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-12-31 12:41:17 +0300

    Adds question for  no repeats challenge

--- challenge-2/01_no_repeats.rb
+++ challenge-2/01_no_repeats.rb
@@ -1,2 +1,11 @@
+# Write a function, `no_repeats(year_start, year_end)`, which takes a
+# range of years and outputs those years which do not have any
+# repeated digits.
+#
+# You should probably write a helper function, `no_repeat?(year)` which
+# returns true/false if a single year doesn't have a repeat.
+#
+# Difficulty: 1/5
+
 def no_repeats(year_start, year_end)
 end
