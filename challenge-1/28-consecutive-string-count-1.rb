5dbc316210f8684d70902e307920bca1858ef6de
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-02-05 00:03:42 +0300

    Fixes formatting issues

--- challenge-1/28-consecutive-string-count-1.rb
+++ challenge-1/28-consecutive-string-count-1.rb
@@ -1,44 +1,44 @@
-# Create a method consecutive_string_count that will take a string as an arguments and 
+# Create a method consecutive_string_count that will take a string as an arguments and
 # return a multi-dimensional array of consecutive letters with their count.
 # "aaabbcaabc" --> [[a, 3], [b, 2], [c,1],[a,2],[b,1],[c,1]
 
 def consecutive_string_count(string)
 
-  # Question: return the sequentialial count of similar characters in a string 
-  # Keyword is sequential and count, the count is very important! 
+  # Question: return the sequentialial count of similar characters in a string
+  # Keyword is sequential and count, the count is very important!
 
-  # Loop over the string scanning each individual character 
+  # Loop over the string scanning each individual character
   # For the first character set its count to one
-  # If its equal to the next character then increment the count by one 
+  # If its equal to the next character then increment the count by one
   # If its equal to the next character then increment the count by one again
-  # If its not equal to the next character, stop the incrementing, add the current character to its 
+  # If its not equal to the next character, stop the incrementing, add the current character to its
   # own minor array, with the current count. Then add this to the main array.
-  # Reset the current count to 1 which will be for the next character to which the current character 
-  # is not equal to. 
+  # Reset the current count to 1 which will be for the next character to which the current character
+  # is not equal to.
   # Repeat the process until you are done with iteration
-  # Finally return the contents of the main array. 
+  # Finally return the contents of the main array.
 
-  main_array = [] 
+  main_array = []
 
-  index = 0 
-  
-  count = 1 
-  
-  string.each_char do |current_char| 
-    
+  index = 0
+
+  count = 1
+
+  string.each_char do |current_char|
+
     if current_char == string[index + 1] # next character
       count += 1
-    else 
+    else
       minor_array = [current_char, count]
-      main_array << minor_array 
-      count = 1 
+      main_array << minor_array
+      count = 1
     end
-  
+
     index += 1
 
-    end
-
-    p main_array
   end
+
+  p main_array
+end
 
 consecutive_string_count("aaabbcaabc")
