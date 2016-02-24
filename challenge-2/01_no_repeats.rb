26db98cc52920b5b82344ee704cff3ced877f69a
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-02-25 02:37:04 +0300

    Adds no_repeat? predicate

--- challenge-2/01_no_repeats.rb
+++ challenge-2/01_no_repeats.rb
@@ -7,5 +7,32 @@
 #
 # Difficulty: 1/5
 
+def no_repeat?(year)
+  # Check for uniqueness of numbers in years
+  # I - Numbers/String
+  # P - # Loop through each number and compare it to another number stored in an array
+        # If the number already exists in the array then there is a repeat return false
+        # If the number does n't exist in the check array then there is no repeat, iterate till the end
+        # and return a true if okay else return a false
+  # O - Boolean
+
+  check_array = []
+
+  year.to_s.each_char do |number|
+    return false if check_array.includes?(number)
+    check_array << number
+  end
+
+  return true
+end
+
+puts no_repeats(1234, 1234) #.should == [1234]
+puts no_repeats(1123, 1123) #.should == []
+puts no_repeats(1980, 1987) #.should == [ 1980, 1982, 1983, 1984, 1985, 1986, 1987]
+
+
+
 def no_repeats(year_start, year_end)
+
+
 end
