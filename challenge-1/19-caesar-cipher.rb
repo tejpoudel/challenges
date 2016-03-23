d0e367c1d30122a8c8bf26a36f7ab88f46ee66d7
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-03-23 03:11:33 +0300

    Adds 3/4 solution to caesar cipher

--- challenge-1/19-caesar-cipher.rb
+++ challenge-1/19-caesar-cipher.rb
@@ -24,6 +24,46 @@
 # information, we would not give it to you on the timed challenge. :-)
 
 def caesar_cipher(offset, string)
+  # Rephrase the question: Move each character three steps forward and replace
+  # the current character with the new one.
+  # Summarize in a sentence i.e. What it wants me to do: Code translation i.e.
+  # create a new alphabet i.e cryptography
+  # Leave spaces as they are.
+  #
+  # Steps without a computer:
+  # 1. Loop through each character in the string
+  # 2. Get the current character's ascii code
+  # 3. Increment the ascii code by the offset.
+  # 4. Get the matching character of the incremented ascii code
+  # 5. Replace the current character with the new matching character.
+  # 6. Repeat the process.
+  # 7. Make sure the space is at the same place in the new string as it was in the old one.
+  # Constraint: The offset number must not exceed 122
+  # If it exceeds 122, subtract 122 from the number,
+  # Add add the result to "a" which is the first character.
+
+  new_string = ""
+
+  # white_space_location = string.index(/\s/)
+
+  string.each_char do |character|
+
+      new_ascii_code = character.ord + offset
+    
+      if new_ascii_code > "z".ord
+        result = new_ascii_code - "z".ord
+        new_character = ("a".ord + result-1).chr
+      else
+        new_character = new_ascii_code.chr
+      end
+
+    new_string += new_character.to_s
+  end
+
+  puts new_string
+
+  # new_string.insert(white_space_location, 32.chr) if !white_space_location.nil?
+
 end
 
 # These are tests to check that your code is working. After writing
