4d10ebbccc6a31baaed750ccd107bb53c1208589
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-03-24 06:47:07 +0300

    Adds overcooked, half baked and simply muddled nothing nobody knows solution which does n't work

--- challenge-1/08-two-sum.rb
+++ challenge-1/08-two-sum.rb
@@ -5,6 +5,52 @@
 # Difficulty: medium.
 
 def two_sum(nums)
+  #
+  # I -Array
+  # Processing
+    # Look at the size of the array
+    # The number of computations/additions will decrease as the index of the number increases
+    # Get the first number, add it to the numbers in the rest of the position
+    # If the sum of any 2 numbers equals zero, return the positions of those numbers
+    # Repeat the same process for each of the rest of numbers
+  # O - Array of Numbers
+
+
+   first_number = 0
+
+   while first_number < nums.size
+
+     next_number = first_number + 1
+
+     while next_number < nums.size
+
+       if first_number + next_number == 0
+         result = [nums.index(first_number), nums.index(next_number)]
+         return results
+       end
+
+       if next_number > 0
+
+         initial_number = 0
+
+         until initial_number == next_number do
+           if next_number + initial_number == 0
+             result = [nums.index(next_number), nums.index(initial_number)]
+             return results
+           end
+
+           initial_number += 1
+         end
+       end
+
+       next_number += 1
+     end
+
+     first_number += 1
+   end
+
+   puts result
+
 end
 
 # These are tests to check that your code is working. After writing
