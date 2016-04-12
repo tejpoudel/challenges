111cb245b92e4b234d65651445d2e42223b66053
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-04-12 23:45:13 +0300

    Adds scramble string solution

--- challenge-1/14-scramble-string.rb
+++ challenge-1/14-scramble-string.rb
@@ -5,6 +5,39 @@
 # Difficulty: medium.
 
 def scramble_string(string, positions)
+
+  # I - string, array
+  # P -
+  # 0 - string
+  #
+  # No constraints
+  #
+  # Summary, what does the question want: Use the array indices to create a new string
+  # Map the indices in the array with the character positions in the string
+  #
+  #
+  # Solve without a computer
+
+  # 1. Get the array
+  # 2. Loop over the array
+  # 3. Get each item in the array
+  # 4. Use each item in the array to retrieve the character in the string at that index
+  # 5. Add that character which has been accessed to a new string
+  # 6. Then repeat the process
+  # 7. Return the new string
+  #
+
+  new_string = ''
+
+  positions.each do |position|
+    string_character = string[position]
+
+    new_string += string_character
+
+  end
+
+  new_string
+
 end
 
 # These are tests to check that your code is working. After writing
