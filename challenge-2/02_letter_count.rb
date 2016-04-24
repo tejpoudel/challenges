fda514e9e711f337eae3b8637d525e67fabe9ee3
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-04-24 12:28:56 +0300

    Add solution for letter count

--- challenge-2/02_letter_count.rb
+++ challenge-2/02_letter_count.rb
@@ -5,4 +5,34 @@
 # Difficulty: 1/5
 
 def letter_count(str)
+  # Count the number of occurences of a letter
+
+# I - string
+# P -
+# O - hash
+
+# Processing
+# Loop over each character in the string
+# Set its count to 1
+# Store the character
+# Continue the loop
+# If the current character already exists in the store
+# Retrieve it and increment the count by one
+# Else store the character and repeat the process all over again
+
+
+  mapper = {}
+
+  str.each_char do |character|
+    if character == " "
+      next
+    elsif mapper.include?(character)
+      mapper[character] += 1
+    else
+      mapper[character] = 1
+    end
+  end
+
+  mapper
+
 end
