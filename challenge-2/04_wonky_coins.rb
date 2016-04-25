6cd3df5b42bc74c37011da4885a593adb47976a6
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-04-25 05:56:15 +0300

    Adds solution for wonky coins

--- challenge-2/04_wonky_coins.rb
+++ challenge-2/04_wonky_coins.rb
@@ -10,4 +10,30 @@
 # Difficulty: 3/5
 
 def wonky_coins(n)
+  # Problem: Return all zero coins from every single denomination i.e divide till you get zero
+  #
+  # Summary: Track the count of zero coins in every denomination given
+  #
+  # I - Integer
+  # P -
+  # O - Integer
+  #
+  # Constraint: Zero denominations have a coin as well. Also round down numbers.
+  #
+  # Process
+  # For each number N divide it 3 times to get coins
+  # First by N/2
+  # Second by N/3
+  # Third by N/4
+  # Store the coins somewhere
+  # Then if each coin is greater than zero repeat the division
+  # Repeat until you end up with zero coins only
+  # Return the results of the number of coins you have produced.
+
+  if n == 0
+    return 1
+  else
+    return wonky_coins(n.fdiv(2).floor) + wonky_coins(n.fdiv(3).floor) + wonky_coins(n.fdiv(4).floor)
+  end
+
 end
