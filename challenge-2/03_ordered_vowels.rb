7606824795377bfeef1543a85307c6dde0625d7c
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-07-01 18:46:04 +0300

    Adds steps for ordered vowels challenge

--- challenge-2/03_ordered_vowels.rb
+++ challenge-2/03_ordered_vowels.rb
@@ -9,4 +9,32 @@
 # Difficulty: 2/5
 
 def ordered_vowel_words(str)
+
+  Summary: Return the words that are match the known order of vowels
+
+  I - String
+  P -
+  O - String
+
+  Kindergaten steps
+
+  1. Loop over each word.
+  2. Call the ordered_vowel_word method on each
+  3. Store the result in a variable
+  4. Return the results of ordered vowel words
+end
+
+def ordered_vowel_word(word)
+
+  Kindergaten steps
+
+  1. Have a container of known vowels in their order
+  2. Loop over the word and check if each character in the word is container in the known list of vowels
+  3. In other words we are checking if the word contains vowels
+  4. Pull out those vowels and store then somewhere
+  5. Create a copy of the known list of vowels, with the none appearing vowels removed
+  6. Then compare the two, the list of vowels from the word, and the edited list of known vowels
+  7. If they match, then return true else return false
+
+  constraints: Need to cater for duplicate vowels
 end
