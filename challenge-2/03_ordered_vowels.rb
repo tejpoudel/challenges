e4a8f8a7928cbfd40ac8d443fa40d1101f5cecd4
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-07-07 00:45:33 +0300

    Adds solution for ordered vowels

--- challenge-2/03_ordered_vowels.rb
+++ challenge-2/03_ordered_vowels.rb
@@ -10,31 +10,56 @@
 
 def ordered_vowel_words(str)
 
-  Summary: Return the words that are match the known order of vowels
+  # Summary: Return the words that are match the known order of vowels
+  #
+  # I - String
+  # P -
+  # O - String
+  #
+  # Kindergaten steps
+  #
+  # 1. Loop over each word.
+  # 2. Call the ordered_vowel_word method on each
+  # 3. Store the result in a variable
+  # 4. Return the results of ordered vowel words
 
-  I - String
-  P -
-  O - String
+  words = str.split
+  ordered_words = []
 
-  Kindergaten steps
+  words.each do |word|
+    if ordered_vowel_word(word)
+      ordered_words << word
+    end
+  end
 
-  1. Loop over each word.
-  2. Call the ordered_vowel_word method on each
-  3. Store the result in a variable
-  4. Return the results of ordered vowel words
+  ordered_words.join(" ")
 end
 
 def ordered_vowel_word(word)
 
-  Kindergaten steps
+  # Checks if each word is ordered
 
-  1. Have a container of known vowels in their order
-  2. Loop over the word and check if each character in the word is container in the known list of vowels
-  3. In other words we are checking if the word contains vowels
-  4. Pull out those vowels and store then somewhere
-  5. Create a copy of the known list of vowels, with the none appearing vowels removed
-  6. Then compare the two, the list of vowels from the word, and the edited list of known vowels
-  7. If they match, then return true else return false
+  # Kindergaten steps
+  #
+  # 1. Have a container of known vowels in their order
+  # 2. Loop over the word and check if each character in the word is container in the known list of vowels
+  # 3. In other words we are checking if the word contains vowels
+  # 4. Pull out those vowels and store them somewhere
+  # 5. Match the vowels_in_word with those in the list of vowels
 
-  constraints: Need to cater for duplicate vowels
+  vowels = ['a', 'e', 'i', 'o', 'u']
+
+  vowels_in_word = []
+
+  word.each_char do |character|
+    if vowels.include?(character)
+      vowels_in_word << character
+    end
+  end
+
+  if vowels_in_word != vowels_in_word.sort
+    return false
+  end
+
+  true
 end
